c97503b0d2b1ae44b7cc17e76769df2386248d30
Roman Slonkin <romanslonkin@gmail.com>
2024-10-16 13:20:43 +0300

    task_1

---
+++ task1/main.py
@@ -0,0 +1,21 @@
+def total_salary(path):
+    """Function to analyze salaries in given file"""
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            salaries = list()
+            for line in lines:
+                salaries.append(int(line.split(',')[1]))
+            total = sum(salaries)
+            average = int(total/len(salaries))
+        
+    except (FileNotFoundError, IOError) :
+            print('File not found or broken!')
+    
+    return total, average
+
+total, average = total_salary("E:/go_it/homework_4/goit-algo-hw-04/task1/salary_file.txt") #test
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average} ") #test
+#EOF
+
+
