8f85a69ec4a4dbb417a56685fd758d35c1fd3669
Roman Slonkin <romanslonkin@gmail.com>
2024-10-18 19:22:36 +0300

    Task3

---
+++ task3/main.py
@@ -0,0 +1,24 @@
+from colorama import Fore
+from pathlib import Path
+import sys
+
+path = sys.argv[1]
+parent_folder_path = Path(path)
+
+def main(path):
+    'Function to analyze directory'
+    try:
+        for element in path.iterdir():
+            if element.is_dir():
+                print (Fore.GREEN + f'{element.name}')
+                main(element)
+            if element.is_file():
+                print(Fore.LIGHTCYAN_EX + f'{element.name}')
+    except (NotADirectoryError, FileNotFoundError):
+        print(Fore.RED + 'There is no such directory!')
+         
+
+main(parent_folder_path)
+
+
+
